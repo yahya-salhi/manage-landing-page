f967650adb7403fb7aa040efd6f0fd8090ea2c46
yahya-salhi <pcclub10@gmail.com>
2022-09-24 00:41:14 +0100

    add some class to nav section part 4

---
+++ main.js
@@ -0,0 +1,8 @@
+const navToggle=document.querySelector(".mobile-nav-toggle")
+const primaryNav=document.querySelector('.primary-navigation')
+
+navToggle.addEventListener('click',()=>{
+  
+  primaryNav.toggleAttribute('data-visible')
+
+})
